8bc8f637601330262369af5804124ad95216b00a
petterpaulo <petterpaulo@gmail.com>
2019-01-26 15:22:10 -0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:climatico/ui/climatico.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+    MaterialApp(
+      home: Climatico(),
+    ));
+
---
+++ lib/util/util.dart
@@ -0,0 +1,2 @@
+final appId = "84de9084f5eb334a71437b5da2535585";
+final minhaCidade = "Itapema";
